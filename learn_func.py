76ea6e4f4a757d389abd8f1c0f2ff15a3354f40b
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2020-04-16 14:35:47 +0300

    develop[init-proj]

--- learn_func.py
+++ learn_func.py
@@ -40,10 +40,11 @@ class TestLay(u.TestCase):
     def test_7(self):
         nn_map = (2, 3, 1)
         X = [[1, 1], [1, 0], [0, 1], [0, 0]]
-        Y = [[1], [1], [1], [0]]
+        Y_or = [[1], [1], [1], [0]]
+        Y_and = [[1], [0], [0], [0]]
         b_c = [0] * bc_bufLen  # буффер для сериализации матричных элементов и входов
         initiate_layers(self.nn_params, nn_map, len(nn_map))
-        learn(b_c,self.nn_params, 0.07, 7, X, Y)
+        learn(b_c,self.nn_params, 0.07, 7, X, Y_and)
         compil_serializ(b_c, self.nn_params.list_, len(nn_map) - 1, "weight2" )
         print("in test_7 after learn matr")
         for i in self.nn_params.list_:
@@ -55,5 +56,5 @@ class TestLay(u.TestCase):
        for i in nn_params1.list_:
           print(i.matrix)
        print(answer_nn_direct(nn_params1, [1, 1], 1))
-       answer_nn_direct_on_contrary(nn_params1, [0], 1)
+       answer_nn_direct_on_contrary(nn_params1, [1], 1)
     # def test_9(self):
