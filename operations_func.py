0c545d1af73b2635db56b706a12bacdbe47c8af1
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2020-04-16 14:41:12 +0300

    develop[copyed_py_file_operations_

--- operations_func.py
+++ operations_func.py
@@ -1,10 +1,17 @@
 from nn_constants import RELU_DERIV, RELU, TRESHOLD_FUNC, TRESHOLD_FUNC_DERIV, LEAKY_RELU, LEAKY_RELU_DERIV,\
-SIGMOID, SIGMOID_DERIV, DEBUG, DEBUG_STR, INIT_W_HE, INIT_W_GLOROT
+SIGMOID, SIGMOID_DERIV, DEBUG, DEBUG_STR, INIT_W_HE, INIT_W_GLOROT_V1, INIT_W_HABR, INIT_W_MY, INIT_W_UNIFORM,\
+    TAN, TAN_DERIV
 import numpy as np
 import math
 np.random.seed(42)
+ready = False
+f=0
 # операции для функций активаций и их производных
 def operations( op,  a,  b,  c,  d,  str):
+    global ready, f
+    alpha = 1.7159
+    beta = 2 / 3
+
     if op == RELU:
         if (a <= 0):
             return 0
@@ -35,10 +42,28 @@ def operations( op,  a,  b,  c,  d,  str):
     elif op == SIGMOID:
         return 2.0 / (1 + math.exp(b * (-a)))
     elif op == SIGMOID_DERIV:
-        return b * 2.0 / (1 + math.exp(b * (-a)))*(1 - 1.0 / (1 + math.exp(b * (-a))))
+        return b * 2.0 / (1 + math.exp(b * (-a)))*(1 - 2.0 / (1 + math.exp(b * (-a))))
     elif op == DEBUG:
         print("%s : %f\n"%( str, a))
+    elif op == INIT_W_HABR:
+        return 2 * np.random.random() - 1
     elif op == INIT_W_HE:
         return np.random.randn() * math.sqrt(2 / a)
+    elif op == INIT_W_MY:
+        if ready:
+            ready = False
+            return -0.01
+        ready = True
+        return 0.01
+    elif op ==INIT_W_GLOROT_V1:
+        return 2 / (a + b)
+    elif op == INIT_W_UNIFORM:
+        print("in op  INIT_W_UNIFORM a=",a,"b=",b)
+        return a + np.random.random() * (b - a)
+    elif op == TAN:
+        f = alpha * math.tanh(beta * a)
+        return  f
+    elif op == TAN_DERIV:
+        return beta / alpha * (alpha * alpha - f * f)
     elif op == DEBUG_STR:
         print("%s\n"%str)
---
+++ py_conf_nn_send.bash
@@ -0,0 +1,6 @@
+cd ../home/msys_u/code/python/py_conf_nn
+pwd
+comm=$1
+git add --all
+git commit -m $comm
+git push origin HEAD
