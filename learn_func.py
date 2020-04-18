bc97bf099f911d5a1138f117cfa169be73d6f96b
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2020-04-18 23:37:04 +0300

    develop[Cv_9_of_16_Cv56]

--- learn_func.py
+++ learn_func.py
@@ -18,7 +18,7 @@ def learn(b_c:list, nn_params, l_r, epochcs, train_set:list, target_set:list):
     n_mse = []
     A = 0.01
     exit_flag = False
-    acc_shurenss = 225
+    acc_shurenss = 400
     acc = 0
     alpha=0.99
     beta=1.01
