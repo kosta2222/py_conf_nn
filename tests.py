5536aaefc651206fbc5be0da2ad99a874f5f0170
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2020-04-30 10:46:52 +0300

    dev[ad-d_gist_to_difficlt_train_40per_eval_RELU]

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ from lear_func import initiate_layers, answer_nn_direct, answer_nn_direct_on_con
 from serial_deserial_func import compil_serializ
 from fit import fit
 import unittest as u
+import numpy as np
 
 
 #----------------------Основные параметры сети----------------------------------
@@ -99,6 +100,56 @@ class TestLay(u.TestCase):
         print("*ON CONTRARY*")
         answer_nn_direct_on_contrary(nn_params_new, [0], 1)
         print("-------------")
+
+    def test_3(self):
+        # устанавливаю параметры
+        nn_params = create_nn_params()
+        nn_params.with_bias = False
+        nn_params.with_adap_lr = True
+        nn_params.lr = 0.01
+        nn_params.act_fu = RELU
+        nn_params.alpha_sigmoid= 0.056
+        nn_map = (3, 2, 3)
+
+        X = [[0, 0, 0], [0, 1, 1], [0, 0, 1], [1, 1, 1],[0, 1, 0]]
+        Y = [[0, 0, 0], [0, 1, 1], [0, 0, 1], [1, 1, 1],[0, 1, 0]]
+        X_gist = [[0, 1/7]]
+        Y_gist = [[0, 1/7]]
+        X_gist_fix = np.zeros((1, 3))
+        Y_gist_fix = np.zeros((1, 3))
+
+        for row in range(1):
+            for elem in range(2):
+               X_gist_fix[row][elem] =\
+                   X_gist[row][elem]
+               Y_gist_fix[row][elem] = Y_gist[row][elem]
+
+        b_c_new = [0] * bc_bufLen  # буффер для сериализации матричных элементов и входов
+        initiate_layers(nn_params, nn_map, len(nn_map))
+        fit(b_c_new, nn_params, 7, X, Y, X, Y, 100)
+
+        print("in test_1 after learn. matr")
+        for i in nn_params.list_:
+            print(i.matrix)
+        # сериализуем
+        compil_serializ(nn_params, b_c_new, nn_params.list_, len(nn_map) - 1, "weight_file" )
+
+        # десериализуем
+        nn_params_new = create_nn_params()
+        deserializ(nn_params_new, nn_params_new.list_, "weight_file")
+
+        print("in test_1 after deserializ. matr")
+        for i in nn_params_new.list_:
+            print(i.matrix)
+
+        # предсказание
+        print(answer_nn_direct(nn_params_new, [1, 1], 1))
+
+        # предсказание наоборот
+        print("*ON CONTRARY*")
+        answer_nn_direct_on_contrary(nn_params_new, [0], 1)
+        print("-------------")
+
 
 
 if __name__ == '__main__':
